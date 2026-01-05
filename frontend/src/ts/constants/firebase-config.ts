ae30d718d4734fb63e6a230a3cd2b25401606b26
Bipin Thapa <bipinas69@gmail.com>
2026-01-05 08:50:31 +0545

    Add Firebase configuration constants

---
+++ frontend/src/ts/constants/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey:  "AIzaSyBlcUAuskfuB0riHr0EAOmkmVMAoeiwmaE",
+  authDomain: "mastermonkey-auth.firebaseapp.com",
+  databaseURL:  "",
+  projectId: "mastermonkey-auth",
+  storageBucket:  "mastermonkey-auth.firebasestorage.app",
+  messagingSenderId: "263257780372",
+  appId: "1:263257780372:web:20588bc96efda29d334481",
+};
